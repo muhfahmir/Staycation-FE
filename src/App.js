f1a63346febcec199916f6a23d25d3e3782f0157
Muhamad Fahmi Rahmatullah <mfr@MacBook-Pro-M1.local>
2021-11-19 15:24:23 +0700

    create components elements InputNumber and InputDate for DetailsPages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./assets/scss/styles.scss";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from "pages/LandingPage";
 import React from "react";
+import Example from "pages/Example";
+import Example2 from "pages/Example2";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPage} />
+        <Route path="/" component={LandingPage} exact />
+        <Route path="/example" component={Example} exact />
+        <Route path="/example2" component={Example2} exact />
       </Router>
     </div>
   );
---
+++ src/pages/Example2.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+import { InputDate } from "elements/Form";
+
+export default class Example2 extends Component {
+  state = {
+    value: {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div
+          className="row align-items-center justify-content-center"
+          style={{ height: "100vh" }}
+        >
+          <div className="col-auto">
+            <InputDate
+              max={30}
+              onChange={this.handleChange}
+              name="value"
+              value={this.state.value}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
